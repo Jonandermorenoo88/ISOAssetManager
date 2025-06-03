01cf0ca4a60b073d1b22fefdc871d8c830513392
jonandermorenoo88 <jaincera97@gmail.com>
2025-06-03 13:26:57 +0200

    creando la pagina de inicio

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/UsuarioController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/UsuarioController.java
@@ -1,19 +1,16 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import es.ubu.gii.ISOAssetManager.repository.UsuarioRepository;
-
-@RestController
+@Controller
 public class UsuarioController {
-	@Autowired
-	private UsuarioRepository usuRepo;
-	
-	@GetMapping("")
-	public String prueba() {
-		return "Inicio";
-	}
 
+    @GetMapping("/")
+    public String mostrarInicio(Model model) {
+        model.addAttribute("usuario", "Jon Ander");
+        return "inicio"; 
+    }
 }
+
