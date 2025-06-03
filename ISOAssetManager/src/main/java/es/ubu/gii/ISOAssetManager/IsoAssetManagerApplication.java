ec89b283b7b1a672a14cc898d6f2a7aa9252bf9d
jonandermorenoo88 <jaincera97@gmail.com>
2025-06-03 09:44:48 +0200

    subiendo el proyecto

---
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/IsoAssetManagerApplication.java
@@ -0,0 +1,13 @@
+package es.ubu.gii.ISOAssetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoAssetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsoAssetManagerApplication.class, args);
+	}
+
+}
