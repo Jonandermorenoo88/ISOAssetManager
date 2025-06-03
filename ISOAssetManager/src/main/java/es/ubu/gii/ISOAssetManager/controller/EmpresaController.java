c98dc2717fd7ee8f080d2714bcfeba07e95b7118
jonandermorenoo88 <jaincera97@gmail.com>
2025-06-04 00:42:36 +0200

    creando el listado de las empresas

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/EmpresaController.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/controller/EmpresaController.java
@@ -1,9 +1,38 @@
 package es.ubu.gii.ISOAssetManager.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.ubu.gii.ISOAssetManager.model.Empresa;
+import es.ubu.gii.ISOAssetManager.repository.EmpresaRepository;
+
+@Controller
 public class EmpresaController {
 
-	public EmpresaController() {
-		// TODO Auto-generated constructor stub
-	}
+    @Autowired
+    private EmpresaRepository empresaRepository;
 
+    @GetMapping("/login/empresas")
+    public String mostrarFormularioEmpresa() {
+        return "empresa";
+    }
+
+    @PostMapping("/login/empresas/nueva")
+    public String registrarEmpresa(@RequestParam String nombre,
+                                   @RequestParam String sector,
+                                   @RequestParam String direccion) {
+
+        Empresa empresa = new Empresa();
+        empresa.setNombre(nombre);
+        empresa.setSector(sector);
+        empresa.setDireccion(direccion);
+
+        empresaRepository.save(empresa);
+
+        return "redirect:/empresas/nueva";
+    }
 }
+
--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/model/Empresa.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/model/Empresa.java
@@ -1,9 +1,60 @@
 package es.ubu.gii.ISOAssetManager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Empresa {
 
-	public Empresa() {
-		// TODO Auto-generated constructor stub
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String sector;
+    private String direccion;
+
+    // Constructor vacío
+    public Empresa() {}
+    
+    public Empresa(String nombre, String sector, String direccion) {
+        this.nombre = nombre;
+        this.sector = sector;
+        this.direccion = direccion;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
 
 }
