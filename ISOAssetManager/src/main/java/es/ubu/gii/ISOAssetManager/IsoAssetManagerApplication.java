9985e4e4adf42957210ddbb2bba46b49e13b16a5
jonandermorenoo88 <jaincera97@gmail.com>
2025-06-11 17:48:31 +0200

    implementando security

--- ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/IsoAssetManagerApplication.java
+++ ISOAssetManager/src/main/java/es/ubu/gii/ISOAssetManager/IsoAssetManagerApplication.java
@@ -16,39 +16,36 @@ import es.ubu.gii.ISOAssetManager.repository.UsuarioRepository;
 @SpringBootApplication
 public class IsoAssetManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IsoAssetManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IsoAssetManagerApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner initData(UsuarioRepository usuarioRepo, RolRepository rolRepo, PasswordEncoder passwordEncoder) {
-	    return args -> {
-	        Rol rolAdmin = rolRepo.findByNombre("ADMIN")
-	            .orElseGet(() -> rolRepo.save(new Rol("ADMIN")));
+    @Bean
+    CommandLineRunner initData(UsuarioRepository usuarioRepo, RolRepository rolRepo, PasswordEncoder passwordEncoder) {
+        return args -> {
+            Rol rolAdmin = rolRepo.findByNombre("ADMIN")
+                .orElseGet(() -> rolRepo.save(new Rol("ADMIN")));
 
-	        if (usuarioRepo.findByEmail("admin@empresa.com").isEmpty()) {
-	            Usuario admin = new Usuario(
-	                "Admin",
-	                "admin@empresa.com",
-	                passwordEncoder.encode("adminpass")
-	            );
-	            admin.setRoles(Set.of(rolAdmin));
-	            usuarioRepo.save(admin);
-	            System.out.println(">>> ADMIN creado con contraseña encriptada.");
-	        }
-	    };
-	}
-	
-	@Bean
-	CommandLineRunner initRoles(RolRepository rolRepo) {
-	    return args -> {
-	        if (rolRepo.findByNombre("AUDITOR").isEmpty()) {
-	            rolRepo.save(new Rol("AUDITOR"));
-	            System.out.println(">>> Rol AUDITOR creado.");
-	        }
-	    };
-	}
+            if (usuarioRepo.findByEmail("admin@empresa.com").isEmpty()) {
+                Usuario admin = new Usuario(
+                    "Admin",
+                    "admin@empresa.com",
+                    passwordEncoder.encode("adminpass")
+                );
+                admin.setRoles(Set.of(rolAdmin));
+                usuarioRepo.save(admin);
+                System.out.println(">>> ADMIN creado con contraseña encriptada.");
+            }
+        };
+    }
 
-
+    @Bean
+    CommandLineRunner initRoles(RolRepository rolRepo) {
+        return args -> {
+            if (rolRepo.findByNombre("AUDITOR").isEmpty()) {
+                rolRepo.save(new Rol("AUDITOR"));
+                System.out.println(">>> Rol AUDITOR creado.");
+            }
+        };
+    }
 }
-
